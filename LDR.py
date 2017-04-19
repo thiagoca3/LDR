aa20d8a49ceb55012827ea85f9d8bdc0b2a6c391
thiagoca3 <thiagoca3@yahoo.com.br>
2017-04-19 17:17:53 -0300

    Add files via upload

---
+++ LDR.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import time
+import RPi.GPIO as GPIO, time, os
+
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+luz = 0
+tempo = 0
+
+def RCtime (RCpin):
+        reading = 0
+        GPIO.setup(RCpin, GPIO.OUT)
+        GPIO.output(RCpin, GPIO.LOW)
+        time.sleep(0.01)
+        GPIO.setup(RCpin, GPIO.IN)
+        while (GPIO.input(RCpin) == GPIO.LOW):
+                reading += 1
+        return reading
+while True:
+        f = open('Results.txt', 'a')
+        luz = RCtime(18)
+        tempo = time.time()
+        f.write('\t' + str(luz) + '\t' + str(tempo) + '\n')
+        f.close()
+
